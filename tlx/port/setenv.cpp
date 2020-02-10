2d390f4a9ae4e980d5be52953785d07f1eb5ec03
Timo Bingmann <tbgit@panthema.net>
2020-02-10 19:57:41 +0100

    port: add MinGW version of setenv()

--- tlx/port/setenv.cpp
+++ tlx/port/setenv.cpp
@@ -3,7 +3,7 @@
  *
  * Part of tlx - http://panthema.net/tlx
  *
- * Copyright (C) 2016-2018 Timo Bingmann <tb@panthema.net>
+ * Copyright (C) 2016-2020 Timo Bingmann <tb@panthema.net>
  *
  * All rights reserved. Published under the Boost Software License, Version 1.0
  ******************************************************************************/
@@ -14,7 +14,7 @@
 
 namespace tlx {
 
-// Windows porting madness because setenv() is apparently dangerous
+// Windows porting madness because setenv() is apparently dangerous.
 #if defined(_MSC_VER)
 
 int setenv(const char* name, const char* value, int overwrite) {
@@ -22,6 +22,17 @@ int setenv(const char* name, const char* value, int overwrite) {
         size_t envsize = 0;
         int errcode = getenv_s(&envsize, nullptr, 0, name);
         if (errcode || envsize) return errcode;
+    }
+    return _putenv_s(name, value);
+}
+
+// More porting weirdness for MinGW (32 and 64)
+#elif defined(__MINGW32__)
+
+int setenv(const char* name, const char* value, int overwrite) {
+    if (!overwrite) {
+        const char* current = getenv(name);
+        if (current) return 0;
     }
     return _putenv_s(name, value);
 }

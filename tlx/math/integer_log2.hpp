69f4de2b3b1794734e4038c3eaefb48d07a2ccc1
Lorenz Hübschle-Schneider <dev@4z2.de>
2021-01-20 13:44:37 +0100

    [math] make ilog2 work with C++11 again

--- tlx/math/integer_log2.hpp
+++ tlx/math/integer_log2.hpp
@@ -13,6 +13,12 @@
 
 namespace tlx {
 
+#if __cplusplus >= 201402L
+    #define TLX_ADVANCED_CONSTEXPR constexpr
+#else
+    #define TLX_ADVANCED_CONSTEXPR inline
+#endif
+
 //! \addtogroup tlx_math
 //! \{
 
@@ -21,7 +27,7 @@ namespace tlx {
 
 //! calculate the log2 floor of an integer type
 template <typename IntegerType>
-static constexpr unsigned integer_log2_floor_template(IntegerType i) {
+static TLX_ADVANCED_CONSTEXPR unsigned integer_log2_floor_template(IntegerType i) {
     unsigned p = 0;
     while (i >= 65536) i >>= 16, p += 16;
     while (i >= 256) i >>= 8, p += 8;
@@ -35,37 +41,37 @@ static constexpr unsigned integer_log2_floor_template(IntegerType i) {
 #if defined(__GNUC__) || defined(__clang__)
 
 //! calculate the log2 floor of an integer type
-static constexpr unsigned integer_log2_floor(int i) {
+static TLX_ADVANCED_CONSTEXPR unsigned integer_log2_floor(int i) {
     if (i == 0) return 0;
     return 8 * sizeof(int) - 1 - __builtin_clz(i);
 }
 
 //! calculate the log2 floor of an integer type
-static constexpr unsigned integer_log2_floor(unsigned int i) {
+static TLX_ADVANCED_CONSTEXPR unsigned integer_log2_floor(unsigned int i) {
     if (i == 0) return 0;
     return 8 * sizeof(unsigned) - 1 - __builtin_clz(i);
 }
 
 //! calculate the log2 floor of an integer type
-static constexpr unsigned integer_log2_floor(long i) {
+static TLX_ADVANCED_CONSTEXPR unsigned integer_log2_floor(long i) {
     if (i == 0) return 0;
     return 8 * sizeof(long) - 1 - __builtin_clzl(i);
 }
 
 //! calculate the log2 floor of an integer type
-static constexpr unsigned integer_log2_floor(unsigned long i) {
+static TLX_ADVANCED_CONSTEXPR unsigned integer_log2_floor(unsigned long i) {
     if (i == 0) return 0;
     return 8 * sizeof(unsigned long) - 1 - __builtin_clzl(i);
 }
 
 //! calculate the log2 floor of an integer type
-static constexpr unsigned integer_log2_floor(long long i) {
+static TLX_ADVANCED_CONSTEXPR unsigned integer_log2_floor(long long i) {
     if (i == 0) return 0;
     return 8 * sizeof(long long) - 1 - __builtin_clzll(i);
 }
 
 //! calculate the log2 floor of an integer type
-static constexpr unsigned integer_log2_floor(unsigned long long i) {
+static TLX_ADVANCED_CONSTEXPR unsigned integer_log2_floor(unsigned long long i) {
     if (i == 0) return 0;
     return 8 * sizeof(unsigned long long) - 1 - __builtin_clzll(i);
 }
@@ -73,32 +79,32 @@ static constexpr unsigned integer_log2_floor(unsigned long long i) {
 #else
 
 //! calculate the log2 floor of an integer type
-static constexpr unsigned integer_log2_floor(int i) {
+static TLX_ADVANCED_CONSTEXPR unsigned integer_log2_floor(int i) {
     return integer_log2_floor_template(i);
 }
 
 //! calculate the log2 floor of an integer type
-static constexpr unsigned integer_log2_floor(unsigned int i) {
+static TLX_ADVANCED_CONSTEXPR unsigned integer_log2_floor(unsigned int i) {
     return integer_log2_floor_template(i);
 }
 
 //! calculate the log2 floor of an integer type
-static constexpr unsigned integer_log2_floor(long i) {
+static TLX_ADVANCED_CONSTEXPR unsigned integer_log2_floor(long i) {
     return integer_log2_floor_template(i);
 }
 
 //! calculate the log2 floor of an integer type
-static constexpr unsigned integer_log2_floor(unsigned long i) {
+static TLX_ADVANCED_CONSTEXPR unsigned integer_log2_floor(unsigned long i) {
     return integer_log2_floor_template(i);
 }
 
 //! calculate the log2 floor of an integer type
-static constexpr unsigned integer_log2_floor(long long i) {
+static TLX_ADVANCED_CONSTEXPR unsigned integer_log2_floor(long long i) {
     return integer_log2_floor_template(i);
 }
 
 //! calculate the log2 floor of an integer type
-static constexpr unsigned integer_log2_floor(unsigned long long i) {
+static TLX_ADVANCED_CONSTEXPR unsigned integer_log2_floor(unsigned long long i) {
     return integer_log2_floor_template(i);
 }
 
@@ -108,37 +114,37 @@ static constexpr unsigned integer_log2_floor(unsigned long long i) {
 // integer_log2_ceil()
 
 //! calculate the log2 floor of an integer type
-static constexpr unsigned integer_log2_ceil(int i) {
+static TLX_ADVANCED_CONSTEXPR unsigned integer_log2_ceil(int i) {
     if (i <= 1) return 0;
     return integer_log2_floor(i - 1) + 1;
 }
 
 //! calculate the log2 floor of an integer type
-static constexpr unsigned integer_log2_ceil(unsigned int i) {
+static TLX_ADVANCED_CONSTEXPR unsigned integer_log2_ceil(unsigned int i) {
     if (i <= 1) return 0;
     return integer_log2_floor(i - 1) + 1;
 }
 
 //! calculate the log2 floor of an integer type
-static constexpr unsigned integer_log2_ceil(long i) {
+static TLX_ADVANCED_CONSTEXPR unsigned integer_log2_ceil(long i) {
     if (i <= 1) return 0;
     return integer_log2_floor(i - 1) + 1;
 }
 
 //! calculate the log2 floor of an integer type
-static constexpr unsigned integer_log2_ceil(unsigned long i) {
+static TLX_ADVANCED_CONSTEXPR unsigned integer_log2_ceil(unsigned long i) {
     if (i <= 1) return 0;
     return integer_log2_floor(i - 1) + 1;
 }
 
 //! calculate the log2 floor of an integer type
-static constexpr unsigned integer_log2_ceil(long long i) {
+static TLX_ADVANCED_CONSTEXPR unsigned integer_log2_ceil(long long i) {
     if (i <= 1) return 0;
     return integer_log2_floor(i - 1) + 1;
 }
 
 //! calculate the log2 floor of an integer type
-static constexpr unsigned integer_log2_ceil(unsigned long long i) {
+static TLX_ADVANCED_CONSTEXPR unsigned integer_log2_ceil(unsigned long long i) {
     if (i <= 1) return 0;
     return integer_log2_floor(i - 1) + 1;
 }

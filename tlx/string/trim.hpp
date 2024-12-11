7386a2e5edc4fa1efacebd11462ddbcd8715fceb
Timo Bingmann <tbgit@panthema.net>
2024-12-11 08:18:06 -0800

    add more tlx::trim() methods for `char`s and `string_view`s (#67)

--- tlx/string/trim.hpp
+++ tlx/string/trim.hpp
@@ -44,6 +44,45 @@ std::string& trim(std::string* str, tlx::string_view drop);
 
 /*!
  * Trims the given string in-place on the left and right. Removes all
+ * characters equal to the given drop character.
+ *
+ * \param str   string to process
+ * \param drop  remove this character
+ * \return      reference to the modified string
+ */
+std::string& trim(std::string* str, char drop);
+
+/*!
+ * Trims the given string in-place on the left and right. Removes all
+ * characters in the given drop array, which defaults to " \r\n\t".
+ *
+ * \param str   string to process
+ * \return      reference to the modified string
+ */
+tlx::string_view& trim(tlx::string_view* str);
+
+/*!
+ * Trims the given string in-place on the left and right. Removes all
+ * characters in the given drop array, which defaults to " \r\n\t".
+ *
+ * \param str   string to process
+ * \param drop  remove these characters
+ * \return      reference to the modified string
+ */
+tlx::string_view& trim(tlx::string_view* str, tlx::string_view drop);
+
+/*!
+ * Trims the given string in-place on the left and right. Removes all
+ * characters equal to the given drop character.
+ *
+ * \param str   string to process
+ * \param drop  remove this character
+ * \return      reference to the modified string
+ */
+tlx::string_view& trim(tlx::string_view* str, char drop);
+
+/*!
+ * Trims the given string in-place on the left and right. Removes all
  * characters in the given drop array, which defaults to " \r\n\t".
  *
  * \param str   string to process
@@ -60,6 +99,16 @@ tlx::string_view trim(tlx::string_view str);
  * \return      reference to the modified string
  */
 tlx::string_view trim(tlx::string_view str, tlx::string_view drop);
+
+/*!
+ * Trims the given string in-place on the left and right. Removes all
+ * characters equal to the given drop character.
+ *
+ * \param str   string to process
+ * \param drop  remove this character
+ * \return      reference to the modified string
+ */
+tlx::string_view trim(tlx::string_view str, char drop);
 
 /******************************************************************************/
 
@@ -83,6 +132,45 @@ std::string& trim_right(std::string* str);
 std::string& trim_right(std::string* str, tlx::string_view drop);
 
 /*!
+ * Trims the given string in-place only on the right. Removes all
+ * characters equal to the given drop character.
+ *
+ * \param str   string to process
+ * \param drop  remove this character
+ * \return      reference to the modified string
+ */
+std::string& trim_right(std::string* str, char drop);
+
+/*!
+ * Trims the given string in-place only on the right. Removes all characters in
+ * the given drop array, which defaults to " \r\n\t".
+ *
+ * \param str   string to process
+ * \return      reference to the modified string
+ */
+tlx::string_view& trim_right(tlx::string_view* str);
+
+/*!
+ * Trims the given string in-place only on the right. Removes all characters in
+ * the given drop array, which defaults to " \r\n\t".
+ *
+ * \param str   string to process
+ * \param drop  remove these characters
+ * \return      reference to the modified string
+ */
+tlx::string_view& trim_right(tlx::string_view* str, tlx::string_view drop);
+
+/*!
+ * Trims the given string in-place only on the right. Removes all
+ * characters equal to the given drop character.
+ *
+ * \param str   string to process
+ * \param drop  remove this character
+ * \return      reference to the modified string
+ */
+tlx::string_view& trim_right(tlx::string_view* str, char drop);
+
+/*!
  * Trims the given string only on the right. Removes all characters in the
  * given drop array, which defaults to " \r\n\t". Returns a copy of the string.
  *
@@ -100,6 +188,16 @@ tlx::string_view trim_right(tlx::string_view str);
  * \return      new trimmed string
  */
 tlx::string_view trim_right(tlx::string_view str, tlx::string_view drop);
+
+/*!
+ * Trims the given string only on the right. Removes all
+ * characters equal to the given drop character.
+ *
+ * \param str   string to process
+ * \param drop  remove this character
+ * \return      new trimmed string
+ */
+tlx::string_view trim_right(tlx::string_view str, char drop);
 
 /******************************************************************************/
 
@@ -123,6 +221,45 @@ std::string& trim_left(std::string* str);
 std::string& trim_left(std::string* str, tlx::string_view drop);
 
 /*!
+ * Trims the given string in-place only on the left. Removes all
+ * characters equal to the given drop character.
+ *
+ * \param str   string to process
+ * \param drop  remove this character
+ * \return      reference to the modified string
+ */
+std::string& trim_left(std::string* str, char drop);
+
+/*!
+ * Trims the given string in-place only on the left. Removes all characters in
+ * the given drop array, which defaults to " \r\n\t".
+ *
+ * \param str   string to process
+ * \return      reference to the modified string
+ */
+tlx::string_view& trim_left(tlx::string_view* str);
+
+/*!
+ * Trims the given string in-place only on the left. Removes all characters in
+ * the given drop array, which defaults to " \r\n\t".
+ *
+ * \param str   string to process
+ * \param drop  remove these characters
+ * \return      reference to the modified string
+ */
+tlx::string_view& trim_left(tlx::string_view* str, tlx::string_view drop);
+
+/*!
+ * Trims the given string in-place only on the left. Removes all
+ * characters equal to the given drop character.
+ *
+ * \param str   string to process
+ * \param drop  remove this character
+ * \return      reference to the modified string
+ */
+tlx::string_view& trim_left(tlx::string_view* str, char drop);
+
+/*!
  * Trims the given string only on the left. Removes all characters in the given
  * drop array, which defaults to " \r\n\t". Returns a copy of the string.
  *
@@ -140,6 +277,16 @@ tlx::string_view trim_left(tlx::string_view str);
  * \return      new trimmed string
  */
 tlx::string_view trim_left(tlx::string_view str, tlx::string_view drop);
+
+/*!
+ * Trims the given string only on the left. Removes all characters equal to the
+ * given drop character. Returns a copy of the string.
+ *
+ * \param str   string to process
+ * \param drop  remove this character
+ * \return      new trimmed string
+ */
+tlx::string_view trim_left(tlx::string_view str, char drop);
 
 //! \}
 //! \}

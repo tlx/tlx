0453d5bdf847fa3259a436a732bce40a5f597e65
Timo Bingmann <tbgit@panthema.net>
2024-12-10 21:57:25 -0800

    string: add index_of(const vector<string_view>&, ...) methods (#66)

--- tlx/string/index_of.cpp
+++ tlx/string/index_of.cpp
@@ -30,7 +30,32 @@ size_t index_of(const std::vector<std::string>& list, tlx::string_view str)
     throw std::runtime_error(reason);
 }
 
+size_t index_of(const std::vector<tlx::string_view>& list, tlx::string_view str)
+{
+    for (size_t i = 0; i < list.size(); ++i)
+    {
+        if (list[i] == str)
+            return i;
+    }
+    std::string reason = "Could not find index_of() ";
+    reason.append(str.begin(), str.end());
+    throw std::runtime_error(reason);
+}
+
 size_t index_of_icase(const std::vector<std::string>& list,
+                      tlx::string_view str)
+{
+    for (size_t i = 0; i < list.size(); ++i)
+    {
+        if (tlx::equal_icase(list[i], str))
+            return i;
+    }
+    std::string reason = "Could not find index_of_icase() ";
+    reason.append(str.begin(), str.end());
+    throw std::runtime_error(reason);
+}
+
+size_t index_of_icase(const std::vector<tlx::string_view>& list,
                       tlx::string_view str)
 {
     for (size_t i = 0; i < list.size(); ++i)
